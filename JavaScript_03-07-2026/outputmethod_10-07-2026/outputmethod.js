b6ddff5112ca9bf7fb71e55fd3038502c0e01973
jayakrishna <jaya.krishna@nybinfotech.com>
2026-07-10 17:51:57 +0530

    added output method

---
+++ JavaScript_03-07-2026/outputmethod_10-07-2026/outputmethod.js
@@ -0,0 +1,50 @@
+//output method in doms
+
+// console.log("hello")
+
+// alert("Hello user!")
+
+document.write("welcome")
+
+//prompt
+
+//  let data=prompt("enter your name")
+//  console.log(data)
+
+let a=document.getElementById("hi")
+let b=document.getElementById("h2")
+console.log(a)
+console.log(b)
+
+
+//bom( browser oject model)
+
+// browser object model
+
+console.log(screen.width)
+console.log(screen.height)
+
+//parseint method 
+// string
+let data=prompt("enter a value")
+// console.log(typeof(data))
+
+//string convert into number we sholud use parseint
+// let data=prompt("enter a value")
+// let m=parseInt(data)
+// console.log(typeof(m))
+
+//parsefloat decimal values( its convert number into string we use to string)
+//  let data=prompt("Enter a value")
+//  let m=parseInt(data)
+
+//  console.log(typeof(m))
+// let k=m.toString()
+//  console.log(typeof(k))
+
+
+//tofixed is use for value 
+let f=12.345677
+console.log(f.toFixed(1))
+
+
